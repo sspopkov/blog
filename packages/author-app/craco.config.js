a9179ccaef2fc94ff542bd7f3f06538c74cd5b6b
Sergey Popkov <sspopkov@list.ru>
2024-10-01 03:01:24 +0300

    build: refine setup and docs

---
+++ packages/author-app/craco.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+// craco.config.js
+module.exports = {
+  webpack: {
+    alias: {
+      '@shared': path.resolve(__dirname, '../shared-components/src/components'),
+    },
+  },
+};
